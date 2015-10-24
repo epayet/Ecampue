799e6fbc1db78a804ee0e4eafbbd23396de9def6
Emmanuel Payet <jakspinning@gmail.com>
2015-10-24 22:04:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 from jak.ecampue.ecampueCalendar import makeCalendar
 
 if __name__ == '__main__':
-    makeCalendar("emmanuel.payet", "271ZAC", "i4-groupeB.ics")
+    makeCalendar("emmanuel.payet", "password", "i4-groupeB.ics")
